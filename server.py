2bc2ff6c3390d2f5e57d850f7becbcac1d998149
user237 <berkozer12@gmail.com>
2023-02-15 17:54:28 +0300

    Add the fastapi script that has a post method that receives a csv file, if successful, returns the first few lines of it in the response, and otherwise, returns an error message.

---
+++ server.py
@@ -0,0 +1,24 @@
+# To run this file, use the following command:
+# uvicorn server:app --host 0.0.0.0 --port 8000
+
+from fastapi import FastAPI, File, UploadFile
+from typing import Optional
+import csv
+
+app = FastAPI()
+
+def csv_process(csv_file):
+    msg = {f"Row {i}": row for i, row in enumerate(csv.reader(csv_file)) if i < 3}
+    return msg
+        
+@app.post("/csv_to_json")
+async def csv_to_json(csv_file: UploadFile = File(...)):
+
+    msg = None
+    if csv_file.content_type != "text/csv":
+        msg = "No csv found"
+    else:
+        contents = await csv_file.read()
+        contents = contents.decode("utf-8").split("\n")
+        msg = csv_process(contents)
+    return {"message": msg}

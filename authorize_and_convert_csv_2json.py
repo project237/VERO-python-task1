06dfcfc1e9ec305705e434d6913ca18c54bb1433
user237 <berkozer12@gmail.com>
2023-02-17 13:34:22 +0300

    Checkpoint until task "For each labelId in the vehicle's JSON array labelIds resolve its colorCode", currently awaiting resolution for the colorCode problem

--- authorize_and_convert_csv_2json.py
+++ authorize_and_convert_csv_2json.py
@@ -1,16 +1,28 @@
 import requests
 import csv
+import os
+import json
 from pprint import pprint
 from server_classes import vehicles
 
-URL_LOGIN   = 'https://api.baubuddy.de/index.php/login'
-URL_QUERY   = 'https://api.baubuddy.de/index.php/v1/vehicles/select/active'
-FILE_NAME   = 'vehicles.csv'
-OUTPUT_FILE = 'api_response.json'
+URL_LOGIN         = 'https://api.baubuddy.de/index.php/login'
+URL_QUERY         = 'https://api.baubuddy.de/index.php/v1/vehicles/select/active'
+URL_COLORS        = 'https://api.baubuddy.de/dev/index.php/v1/labels/' # this will be concatenated with the labelId
+FILE_NAME         = 'vehicles.csv'
+OUTPUT_DIR        = 'output'
+OUTPUT_FILE       = 'output_server.json'
+API_RESPONSE_FILE = 'api_response2.json'
+
+output_dir = os.path.join(os.path.dirname(__file__), OUTPUT_DIR)
+
+file_paths = {
+    'output_file'       : os.path.join(output_dir, OUTPUT_FILE),
+    'api_response_file' : os.path.join(output_dir, API_RESPONSE_FILE)
+}
 
 headers = {
     'Authorization': 'Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz',
-    'Content-Type': 'application/json'
+    'Content-Type' : 'application/json'
 }
 
 data = {
@@ -18,13 +30,33 @@ data = {
     'password': '1'
 }
 
+def resolve_colorCode(labelId: str, headers: dict, data: dict):
+    """
+    Takes a labelId and returns the colorCode of the corresponding label.
+    """
+    color_url = URL_COLORS + labelId
+    print()
+    print("color_url: ", color_url)
+    response = requests.get(color_url, headers=headers, json=data)
+    response_json = response.json()[0] # response is a list of dicts (one dict in this case)
+    pprint(response_json)
+    colorCode = response_json['colorCode']
+    # reassign to none if the return value is empty string
+    if colorCode == "":
+        colorCode = None
+    return colorCode
+
 if __name__ == "__main__": 
 
-    response_login = requests.post(URL_LOGIN, headers=headers, json=data)
-    response_query = requests.get(URL_QUERY)
-    # this is a list of dictionaries
-    response_query_json = response_query.json()
-    # pprint(response_query_json)
+    # logging in and getting the access token
+    new_access_token = requests.post(URL_LOGIN, headers=headers, json=data).json()["oauth"]["access_token"]
+    # new_access_token = response_login.json()["oauth"]["access_token"]
+
+    # setting the new access token in the headers
+    headers["Authorization"] = "Bearer " + new_access_token
+
+    response_query = requests.get(URL_QUERY, headers=headers, json=data)
+    response_query_json = response_query.json() # this is a list of dictionaries
 
     # INITIALIZING OBJECTS AND SETTING THE CSV ATTRIBUTES
 
@@ -33,6 +65,7 @@ if __name__ == "__main__":
     # initialize the vehicles objects and populate the csv attributes
     with open(FILE_NAME, 'r', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+        next(reader) # skip header row
         for row in reader:
             vehicle            = vehicles()
             key                = vehicle.get_csv_attibutes(row)
@@ -52,10 +85,25 @@ if __name__ == "__main__":
     print(len(vehicles_dict))
     print()
     # pprint(vars(vehicles_dict))
-    for e in vehicles_dict.values():
-        pprint(vars(e))
 
+    # RESOLVING THE COLOR CODES AND DELETING THE ONES WITH NONE HU
+    for k, vehicle in vehicles_dict.copy().items():
+        # # delete ones with None hu
+        # if vehicle.hu == None:
+        #     del vehicles_dict[k]
+        #     continue
+        # resolve colorCode
+        if vehicle.labelIds != None:
+            vehicle.colorCode = resolve_colorCode(vehicle.labelIds, headers, data)
+
+    # SAVE THE VEHICLES_DICT TO A JSON FILE
+    vehicles_list = list(vehicles_dict.values())
+    json_data = json.dumps([vars(v) for v in vehicles_list], indent=4, ensure_ascii=False)
+    with open(file_paths['output_file'], 'w') as f:
+        f.write(json_data)
 
     # Use this to write the response to a file
-    # with open(OUTPUT_FILE, 'w') as f:
-    #     f.write(response_query.text)
+    with open(file_paths['api_response_file'], 'w') as f:
+        f.write(response_query.text)
+
+

fa260d02c9d9355afec92cac79c26146138a9cbd
user237 <berkozer12@gmail.com>
2023-02-20 19:20:53 +0300

    Last commit before removing sketch client file from index

--- send_csv_2server.py
+++ send_csv_2server.py
@@ -1,18 +1,16 @@
 import requests
 from pprint import pprint
 
-URL = "http://localhost:8000/csv_to_json"
-FILE = "vehicles.csv"
-FILE2 = "dummy.txt"
+URL        = "http://localhost:8000/csv_to_json"
+INPUT_FILE  = "vehicles.csv"
+FILE2      = "dummy.txt" # for testing purposes
 
 # Open the CSV file and prepare the request
-with open(FILE, "rb") as csv_file:
-    files = {"csv_file": csv_file}
-    response = requests.post(URL, files=files)
+with open(INPUT_FILE, "rb") as csv_file:
+    file_dict = {"csv_file": csv_file}
+    response = requests.post(URL, files=file_dict)
 
 # Print the response
 print()
 pprint(response.json())
 # pprint(response.content)
-
-

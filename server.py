9220305fd04e71852025bbb668cb9fc1f7fac008
user237 <berkozer12@gmail.com>
2023-02-20 14:30:40 +0300

    Server task complete

--- server.py
+++ server.py
@@ -1,25 +1,25 @@
 # To run this file, use the following command:
-# uvicorn server:app --host 0.0.0.0 --port 8000 --reload
-# launch this on http://localhost:8000/csv_to_json
+#   uvicorn server:app --host 0.0.0.0 --port 8000 --reload
+# Launch this on 
+#   http://localhost:8000/csv_to_json
 
-from fastapi import FastAPI, File, UploadFile
-from typing import Optional
-import csv
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from server_classes import vehicles_handler
 
 app = FastAPI()
 
-def csv_process(csv_file):
-    msg = {f"Row {i}": row for i, row in enumerate(csv.reader(csv_file)) if i < 3}
-    return msg
-        
 @app.post("/csv_to_json")
 async def csv_to_json(csv_file: UploadFile = File(...)):
 
-    msg = None
     if not csv_file.filename.endswith(".csv"):
-        msg = "No csv found"
-    else:
-        contents = await csv_file.read()
-        contents = contents.decode("utf-8").split("\n")
-        msg = csv_process(contents)
-    return {"message": msg}
+        raise HTTPException(status_code=422, detail="Request must contain a CSV file.")
+    
+    csv_contents = await csv_file.read()
+    csv_contents = csv_contents.decode("utf-8").split("\n")
+
+    # this is a simple list of dictionaries but will be serialized to JSON when returned by the API
+    processed_json = vehicles_handler().main(csv_contents, filter_hu=True)
+
+    return processed_json
+    
+
--- server_classes.py
+++ server_classes.py
@@ -1,29 +1,108 @@
 
+import requests
 import csv
+from pprint import pprint
 
-FILE_NAME = 'vehicles.csv'
+FILE_NAME   = "vehicles.csv" # for testing
 
 class vehicles_handler:
-    def __init__(self):
-        pass
+    """
+    In this class, we organize the code that processes vehicles.csv and returns a json file that will be sent to the client.
 
-    def request_vehicle_info():
-        pass
+    No init method since this class is only going to have a single instance. So we are using class variables instead of instance variables.
+    """
+    url_login   = 'https://api.baubuddy.de/index.php/login'
+    url_query   = 'https://api.baubuddy.de/index.php/v1/vehicles/select/active'
+    url_colors  = 'https://api.baubuddy.de/dev/index.php/v1/labels/' # this will be concatenated with the labelId
+    filter_hu   = None
+    request_headers = {
+    'Authorization': 'Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz',
+    'Content-Type' : 'application/json'
+    }
+    reuqest_json = {
+        'username': '365',
+        'password': '1'
+    }
+    # these will be set during the execution
+    csv_file            = None
+    response_query_json = None
+    json_data           = None
+    vehicles_dict       = {}
 
-    def populate_csv_attributes():
-        pass
+    def main(self, csv_file, filter_hu: bool = False):
+        self.filter_hu = filter_hu
+        self.csv_file = csv_file
+        self.request_vehicle_info()
+        self.populate_vehicle_attributes()
+        self.filter_hu_and_resolve_colorCodes()
+        return self.json_data
 
-    def populate_json_attributes():
-        pass
+    def request_vehicle_info(self):
+        """
+        Authorizes and requests vehicle info from url_query endpoint.
+        """
+        # logging in and getting the access token
+        new_access_token = requests.post(self.url_login, headers=self.request_headers, json=self.reuqest_json).json()["oauth"]["access_token"]
 
-    def filter_hu_none():
-        pass
+        # setting the new access token in the headers
+        self.request_headers["Authorization"] = "Bearer " + new_access_token
 
-    def resolve_colorCode():
-        pass
+        response_query = requests.get(self.url_query, headers=self.request_headers, json=self.reuqest_json)
+        self.response_query_json = response_query.json() # this is a list of dictionaries
 
-    def save_to_files():
-        pass
+    def populate_vehicle_attributes(self):
+        """
+        Only called by reolve_colorCode.
+        Here we populate the vehicles objects with the attributes from the csv file and the api response (json).
+        """
+        reader = csv.reader(self.csv_file, delimiter=';', quotechar='"')
+        next(reader) # skip header row
+        for row in reader:
+            vehicle            = vehicles()
+            key                = vehicle.get_csv_attibutes(row)
+            self.vehicles_dict[key] = vehicle
+
+        # pass each dict in the list by calling set_json_attributes on the the object with same key
+        for response_dict in self.response_query_json:
+            key = response_dict['kurzname']
+            try:
+                self.vehicles_dict[key].set_json_attributes(response_dict)
+            except KeyError:
+                continue
+
+    def resolve_colorCode(self, labelId):
+        """
+        Takes a labelId and returns the colorCode of the corresponding label.
+        """
+        color_url = self.url_colors + labelId
+        print()
+        print("color_url: ", color_url)
+
+        response = requests.get(color_url, headers=self.request_headers, json=self.reuqest_json)
+        response_json = response.json()[0] # response is a list of dicts (one dict in this case)
+
+        pprint(response_json)
+        colorCode = response_json['colorCode']
+        
+        # reassign to none if the return value is empty string
+        if colorCode == "":
+            colorCode = None
+        return colorCode
+    
+    def filter_hu_and_resolve_colorCodes(self):
+        for k, vehicle in self.vehicles_dict.copy().items():
+            # delete ones with None hu
+            if self.filter_hu and vehicle.hu == None:
+                del self.vehicles_dict[k]
+                continue
+            # resolve colorCode
+            if vehicle.labelIds != None:
+                vehicle.colorCode = self.resolve_colorCode(vehicle.labelIds)
+        
+        self.json_data = list(self.vehicles_dict.values())
+        # vehicles_list = list(self.vehicles_dict.values())
+        # self.json_data = json.dumps([vars(v) for v in vehicles_list], indent=4, ensure_ascii=False)
+    
 
 class vehicles:
     """
@@ -111,21 +190,33 @@ class vehicles:
 if __name__ == "__main__":
     vehicles_dict = {}
 
-    # initialize the vehicles objects and populate the csv attributes
-    with open(FILE_NAME, 'r', newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=';', quotechar='"')
-        for row in reader:
-            vehicle            = vehicles()
-            key                = vehicle.get_csv_attibutes(row)
-            vehicles_dict[key] = vehicle
+    # TEST CODE FOR CLASS vehicles
+    # # initialize the vehicles objects and populate the csv attributes
+    # with open(FILE_NAME, 'r', newline='') as csvfile:
+    #     reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+    #     for row in reader:
+    #         vehicle            = vehicles()
+    #         key                = vehicle.get_csv_attibutes(row)
+    #         vehicles_dict[key] = vehicle
 
-    for i, e in enumerate(vehicles_dict.values()):
-        if i == 12:
-            break
-        print()
-        print("gruppe: ", e.gruppe)
-        print("kurzname: ", e.kurzname)
-        print("langtext: ", e.langtext)
-        print("info: ", e.info)
-        print("lagerort: ", e.lagerort)
-        print("labelIds: ", e.labelIds)
+    # for i, e in enumerate(vehicles_dict.values()):
+    #     if i == 12:
+    #         break
+    #     print()
+    #     print("gruppe: ", e.gruppe)
+    #     print("kurzname: ", e.kurzname)
+    #     print("langtext: ", e.langtext)
+    #     print("info: ", e.info)
+    #     print("lagerort: ", e.lagerort)
+    #     print("labelIds: ", e.labelIds)
+
+    # TEST CODE FOR CLASS vehicles_handler
+    vh = vehicles_handler()
+    output = None
+
+    with open(FILE_NAME, 'r', newline='') as csvfile:
+        output = vh.main(csvfile, filter_hu=True)
+
+    # save output file as "vehicles_handler_output.json"
+    with open("output/vehicles_handler_output.json", "w", encoding="utf-8") as f:
+        f.write(output)
